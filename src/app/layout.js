1154d95e0cee6670bf7272d3c99336a182548dd9
vincenzocaruso <enzo@carusovincenzo.com>
2023-11-02 22:34:29 +0100

    Partial section 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/Header'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
      
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+        <Header />
+        {/* Navbar */}
+        {/* Search */}
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import MenuItem from './MenuItem'
+import { AiFillHome} from 'react-icons/ai'
+
+export default function Header() {
+  return (
+    <div>
+      <div className="">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+      </div>
+      <div className=""></div>
+    </div>
+  )
+}
